f7fa110f59c8980dbcef92e58c18aac25ee139b6
benenov <hardymilalu@gmail.com>
2019-05-02 17:23:09 +0200

    Update book.js
    Ajout du premier test unitaire

--- src/test/book.js
+++ src/test/book.js
@@ -30,8 +30,39 @@ describe('/GET book', () => {
         expect(res.body).to.be.an('object');
         expect(res).to.have.status(200);
         expect(res.body.books).to.be.an('array');
-        expect(res.body.books.length).to.equal(0);
+        //expect(res.body.books.length).to.equal(0);
         done();
+      });
+  });
+});
+
+/**
+ * Premier test unitaire
+ */
+
+describe('/GET book', () => {
+  it('it should GET all the books', done => {
+
+    nock(localhost)
+    .get('/book')
+    .reply(200,{
+        books: [
+            {
+            id: '0db0b43e-dddb-47ad-9b4a-e5fe9ec7c2a9', title: 'Coco raconte Channel 2',
+            years: 1990,
+            pages: 400
+            } ]
+    });
+
+      chai
+      .request(localhost)
+      .get('/book')
+      .end((err, res) => {
+          expect(res.body).to.be.an('object');
+          expect(res).to.have.status(200);
+          expect(res.body.books).to.be.an('array');
+          //expect(res.body.books.length).to.equal(0);
+          done();
       });
   });
 });
